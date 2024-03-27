5684f54a448f2f871b653a7c189db61635e5d443
Mikael Gross <contact@mikaelgross.com>
2024-03-27 18:12:48 +0000

    FIX: extracted categroy card into component

--- app/category/[category]/page.tsx
+++ app/category/[category]/page.tsx
@@ -1,6 +1,6 @@
-import { getPartialData } from "@/lib/api";
 import { Metadata } from "next";
-import Link from "next/link";
+import ArticleCard from "@/components/ArticleCard";
+import { getPartialData } from "@/lib/api";
 
 export async function generateMetadata({ params }: any): Promise<Metadata> {
   const { category } = params;
@@ -17,30 +17,8 @@ export default function CategoryPage({ params }: any) {
     <main>
       <h2>{category}</h2>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-        {dataSets.map((data) => (
-          <Link href={`/category/${category}/${data.slug}`} key={data.slug}>
-            <div className="flex flex-col border-2 dark:border-0 border-[rgb(var(--accent))] dark:bg-[rgb(var(--bg-contrast))] hover:cursor-pointer hover:rotate-1 text-sm text-left active:scale-100 rounded-xl overflow-hidden transition-all">
-              <div className="p-4 pb-0">
-                <h4 className="mb-0 text-[rgb(var(--foreground-rgb))]">
-                  {data.name}
-                </h4>
-              </div>
-              <div className="p-4 text-[rgb(var(--foreground-rgb))]">
-                {data.desc}
-                <hr className="my-2" />
-                {data.submitters.map((submitter: string, key: any) => (
-                  <span key={key}>
-                    <strong>
-                      {submitter}
-                      {key < data.submitters.length - 1 ? ", " : ""}
-                    </strong>
-                  </span>
-                ))}{" "}
-                <br />
-                {data.dateSubmitted}
-              </div>
-            </div>
-          </Link>
+        {dataSets.map((data, key) => (
+          <ArticleCard category={category} data={data} key={key} />
         ))}
       </div>
     </main>
--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,1 +1,29 @@
-export default function ArticleCard() {}
+import Link from "next/link";
+
+export default function ArticleCard({ category, data }: any) {
+  return (
+    <Link href={`/category/${category}/${data.slug}`} key={data.slug}>
+      <div className="flex flex-col border-2 dark:border-0 border-[rgb(var(--accent))] dark:bg-[rgb(var(--bg-contrast))] hover:cursor-pointer hover:rotate-1 text-sm text-left active:scale-100 rounded-xl overflow-hidden transition-all">
+        <div className="p-4 pb-0">
+          <h4 className="mb-0 text-[rgb(var(--foreground-rgb))]">
+            {data.name}
+          </h4>
+        </div>
+        <div className="p-4 text-[rgb(var(--foreground-rgb))]">
+          {data.desc}
+          <hr className="my-2" />
+          {data.submitters.map((submitter: string, key: any) => (
+            <span key={key}>
+              <strong>
+                {submitter}
+                {key < data.submitters.length - 1 ? ", " : ""}
+              </strong>
+            </span>
+          ))}{" "}
+          <br />
+          {data.dateSubmitted}
+        </div>
+      </div>
+    </Link>
+  );
+}
